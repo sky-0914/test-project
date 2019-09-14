bc389ef335a1ce460ed1178f4b73b2ed4bec0092
sky <452425952@qq.com>
2019-09-14 16:13:03 +0800

    order订单服务

---
+++ spring-cloud-greenwich/service/order/src/main/java/com/happyloves/zc/service/order/OrderApplication.java
@@ -0,0 +1,18 @@
+package com.happyloves.zc.service.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@MapperScan(basePackages = "com.happyloves.zc")
+//@EnableEurekaClient
+@EnableDiscoveryClient
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
